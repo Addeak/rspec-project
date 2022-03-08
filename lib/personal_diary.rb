9fdac6c148878754db527480633c198096a85dd4
Adam Deakin <adeakin89@gmail.com>
2022-03-08 11:52:43 +0000

    startiing to class test drive personal diary.

--- lib/personal_diary.rb
+++ lib/personal_diary.rb
@@ -1,9 +1,40 @@
-def make_snippet(str)
-  arr = str.split(" ")
-  new_arr = arr.select.with_index { |word, i| i < 5 }
-  new_arr.join(" ") + "..."
-end
+# def make_snippet(str)
+#   arr = str.split(" ")
+#   new_arr = arr.select.with_index { |word, i| i < 5 }
+#   new_arr.join(" ") + "..."
+# end
 
-def count_words(str)
-  str.split.count
+class DiaryEntry
+  def initialize(title, contents) # title, contents are strings
+    # ...
+  end
+
+  def title
+    # Returns the title as a string
+  end
+
+  def contents
+    # Returns the contents as a string
+  end
+
+  def count_words
+    @contents.split.count
+  end
+
+  def reading_time(wpm) # wpm is an integer representing the number of words the
+                        # user can read per minute
+    # Returns an integer representing an estimate of the reading time in minutes
+    # for the contents at the given wpm.
+  end
+
+  def reading_chunk(wpm, minutes) # `wpm` is an integer representing the number
+                                  # of words the user can read per minute
+                                  # `minutes` is an integer representing the
+                                  # number of minutes the user has to read
+    # Returns a string with a chunk of the contents that the user could read
+    # in the given number of minutes.
+    # If called again, `reading_chunk` should return the next chunk, skipping
+    # what has already been read, until the contents is fully read.
+    # The next call after that it should restart from the beginning.
+  end
 end
