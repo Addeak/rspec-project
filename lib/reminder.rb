aea1cb2aff3c2786c6583ca524aef87beda16e48
Adam Deakin <adeakin89@gmail.com>
2022-03-09 12:01:53 +0000

    Reminder class updated with complete method.

--- lib/reminder.rb
+++ lib/reminder.rb
@@ -8,6 +8,12 @@ class Reminder
     @tasks << task
   end
 
+  def complete(task)
+    fail "Task cannot be an empty string." if task == ""
+    fail "Task has not been set." unless @tasks.include?(task)
+    @tasks.delete(task)
+  end
+
   def remind_me
     fail "No tasks have been set." if @tasks == []
     @tasks.join(", ")
--- spec/reminder_spec.rb
+++ spec/reminder_spec.rb
@@ -31,4 +31,29 @@ RSpec.describe Reminder do
       expect { reminder.add("") }.to raise_error "Task cannot be an empty string."
     end
   end
+
+  context "given a completed task" do
+    it "removes the task from task list" do
+      reminder = Reminder.new
+      reminder.add("Clean kitchen")
+      reminder.add("Meal prep")
+      reminder.complete("Clean kitchen")
+      expect(reminder.remind_me).to eq "Meal prep"
+    end
+  end
+
+  context "given an empty string" do
+    it "raises an error" do
+      reminder = Reminder.new
+      expect { reminder.complete("") }.to raise_error "Task cannot be an empty string."
+    end
+  end
+
+  context "given a completed task that has not been set" do
+    it "raises an error" do
+      reminder = Reminder.new
+      reminder.add("Clean kitchen")
+      expect { reminder.complete("Meal prep") }.to raise_error "Task has not been set."
+    end
+  end
 end
