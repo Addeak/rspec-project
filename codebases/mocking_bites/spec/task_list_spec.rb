803c5eda9ec981f9b8c097b725ce5c7bbd2a128f
Khushnood Asif <khushnood99@gmail.com>
2022-03-14 15:41:55 +0000

    mocking bites challenge complete

--- codebases/mocking_bites/spec/task_list_spec.rb
+++ codebases/mocking_bites/spec/task_list_spec.rb
@@ -12,4 +12,33 @@ RSpec.describe TaskList do
   end
 
   # Unit test `#all` and `#all_complete?` behaviour
+
+  context "after adding tasks" do 
+    it "returns all tasks" do
+      task_list = TaskList.new
+      task = double(:task)
+      task_list.add(task)
+      expect(task_list.all).to eq [task]
+    end
+
+    it "returns true if all tasks are complete" do
+      task_list = TaskList.new
+      task = double(:faketask, complete?: true)
+      task_list.add(task)
+      expect(task_list.all_complete?).to eq true
+    end
+
+    it "returns false if some tasks not complete" do
+      task_list = TaskList.new
+      task = double(:faketask, complete?: true)
+      task_1 = double(:faketask, complete?: false)
+      task_2 = double(:faketask, complete?: true)
+      task_list.add(task)
+      task_list.add(task_1)
+      task_list.add(task_2)
+      expect(task_list.all_complete?).to eq false
+    end
+  end  
 end
+
+
